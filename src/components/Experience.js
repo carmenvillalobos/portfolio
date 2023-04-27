d1f2d688163a0d95d60917f3a2e3f90913c66681
carmenvillalobos <carmvilla15@gmail.com>
2023-04-27 14:21:03 -0700

    changed the background color and more

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -33,11 +33,11 @@ class Experience extends Component {
             className="vertical-timeline-element--work"
             date={work.years}
             iconStyle={{
-              background: "#AE944F",
+              background: "#85ACD6",
               color: "#fff",
               textAlign: "center",
             }}
-            icon={<i className="fab fa-angular experience-icon"></i>}
+            icon={<i className=""></i>}
             key={i}
           >
             <div style={{ textAlign: "left", marginBottom: "4px" }}>
@@ -78,7 +78,7 @@ class Experience extends Component {
             {work}
             <VerticalTimelineElement
               iconStyle={{
-                background: "#AE944F",
+                background: "#85ACD6",
                 color: "#fff",
                 textAlign: "center",
               }}
