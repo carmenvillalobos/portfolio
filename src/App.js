3c7dde465caf16afc58fc7a0201a668703a7e0be
carmenvillalobos <carmvilla15@gmail.com>
2023-05-04 09:02:46 -0700

    added a navigation bar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import NavBar from "./components/NavBar";
 
 class App extends Component {
 
@@ -82,6 +83,8 @@ class App extends Component {
   render() {
     return (
       <div>
+        <NavBar />
+
         <Header sharedData={this.state.sharedData.basic_info} />
         <div className="col-md-12 mx-auto text-center language">
           <div
